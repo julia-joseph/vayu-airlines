1c7c6b0248c7d91994c05f262241378a066145f9
Julia <juljoe726@gmail.com>
2020-05-02 19:40:55 +0530

    css changes for flight-search

--- src/app/components/flight-search/flight-search.module.ts
+++ src/app/components/flight-search/flight-search.module.ts
@@ -2,10 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { FlightSearchComponent } from './flight-search.component';
-import { FlightSearchNavbarComponent } from './flight-search-navbar/flight-search-navbar.component';
-import { FlightSearchHeaderComponent } from './flight-search-header/flight-search-header.component';
 import { FlightSearchContentComponent } from './flight-search-content/flight-search-content.component';
-import { FlightSearchFooterComponent } from './flight-search-footer/flight-search-footer.component';
 import { BookFlightComponent } from './flight-search-content/book-flight/book-flight.component';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
@@ -13,10 +10,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 @NgModule({
   declarations: [
     FlightSearchComponent,
-    FlightSearchNavbarComponent,
-    FlightSearchHeaderComponent,
     FlightSearchContentComponent,
-    FlightSearchFooterComponent,
     BookFlightComponent
   ],
   imports: [
