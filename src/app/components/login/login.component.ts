8c45a406df23991788ef9d00c92f592d1cf7b0c7
Julia <juljoe726@gmail.com>
2020-05-07 15:59:17 +0530

    Pritee - Login Validation

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 
 @Component({
@@ -8,15 +9,29 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+  submitted = false;
 
-  constructor( private router: Router) { }
+  constructor(private router: Router, private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
-  }
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]]
 
-  onSubmit(value) {
-    console.log(value);
-    this.router.navigate(['/flight-search']);
+    });
+  }
+  // convenience getter for easy access to form fields
+  get f() { return this.loginForm.controls; }
+
+  onSubmit() {
+    this.submitted = true;
+    if (this.loginForm.valid) {
+
+      this.router.navigate(['/flight-search']);
+
+    }
+
   }
 
 }
